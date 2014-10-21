9ab2824b8a9acf083c32eccc3b24eb00756df32b
Aid Farhan Maarof <aidfarhan@gmail.com>
2014-10-21 21:55:50 +0800

    enable reboot bootloader in twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,6 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_NO_SCREEN_BLANK := true
-TW_NO_REBOOT_BOOTLOADER := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 PRODUCT_COPY_FILES += device/lge/palman/twrp.fstab:recovery/root/etc/twrp.fstab
 
